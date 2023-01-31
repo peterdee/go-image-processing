e5771fa1ecec68d18c422d8ed6b84f37fcc1927e
Peter <peter.dyumin@gmail.com>
2023-01-31 21:15:04 +0300

    Work on optimized Sobel filter

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ import (
 var FORMAT string
 
 func main() {
-	path := "images/9.jpeg"
+	path := "images/5.png"
 	img, f, openMS, convertMS := utilities.OpenFile(path)
 	FORMAT = f
 	now := math.Round(float64(time.Now().UnixNano()) / 1000000)
@@ -23,26 +23,26 @@ func main() {
 	// rotate90 := processing.Rotate90(img)
 	// rotate270 := processing.Rotate270(img)
 	// rotate180 := processing.Rotate180(img)
-	// sobel := processing.SobelFilter(img)
+	sobel := processing.SobelFilter(img)
 	// emboss := processing.EmbossFilter(img)
 	// rotateN := processing.RotateAngle(img, 52)
 	// kuwahara := processing.KuwaharaFilter(img, 5)
 	// gauss := processing.GaussianBlur(img, 25)
 	// laplasian := processing.LaplasianFilter(img)
-	sharpen := processing.SharpenFilter(img, 99)
+	// sharpen := processing.SharpenFilter(img, 99)
 	processMS := int(math.Round(float64(time.Now().UnixNano())/1000000) - now)
 	// println(est)
 	name := fmt.Sprintf(`file-%d.%s`, time.Now().Unix(), FORMAT)
 	// utilities.SaveFile("flippedV-"+name, FORMAT, flippedV)
 	// utilities.SaveFile("flippedH-"+name, FORMAT, flippedH)
 	// save("rotate90-"+name, rotate90)
-	// utilities.SaveFile("sobel-"+name, FORMAT, sobel)
+	saveMS := utilities.SaveFile("sobel-"+name, FORMAT, sobel)
 	// utilities.SaveFile("emboss-"+name, FORMAT, emboss)
 	// utilities.SaveFile("rotateN-"+name, FORMAT, rotateN)
 	// saveMS := utilities.SaveFile("kuwahara-"+name, FORMAT, kuwahara)
 	// utilities.SaveFile("gauss-"+name, FORMAT, gauss)
 	// utilities.SaveFile("laplasian-"+name, FORMAT, laplasian)
-	saveMS := utilities.SaveFile("sharp-"+name, FORMAT, sharpen)
+	// saveMS := utilities.SaveFile("sharp-"+name, FORMAT, sharpen)
 	// save("rotate180-"+name, rotate180)
 	// save("rotate270-"+name, rotate270)
 	// utilities.SaveFile("box-blur-"+name, FORMAT, boxBlur)
@@ -51,7 +51,7 @@ func main() {
 
 	// optimized.Binary(path, 185)
 	// optimized.Brightness(path, 56)
-	optimized.Contrast(path, 225)
+	// optimized.Contrast(path, 225)
 	// optimized.EightColors(path)
 	// optimized.GammaCorrection(path, 0.7)
 	// optimized.Grayscale(path, constants.GRAYSCALE_TYPE_LUMINOSITY)
@@ -59,6 +59,7 @@ func main() {
 	// optimized.Invert(path)
 	// optimized.Sepia(path)
 	// optimized.Solarize(path, 175)
+	optimized.Sobel(path)
 
 	sum := openMS + convertMS + processMS + saveMS
 	println("open", openMS, "convert", convertMS, "process", processMS, "save", saveMS, "sum", sum)
---
+++ optimized/sobel.go
@@ -0,0 +1,62 @@
+package optimized
+
+import (
+	"math"
+	"time"
+)
+
+var sobelHorizontal = [3][3]int{
+	{-1, 0, 1},
+	{-2, 0, 2},
+	{-1, 0, 1},
+}
+
+var sobelVertical = [3][3]int{
+	{1, 2, 1},
+	{0, 0, 0},
+	{-1, -2, -1},
+}
+
+func getCoordinates(pixel, width int) (int, int) {
+	return pixel % width, int(math.Floor(float64(pixel) / float64(width)))
+}
+
+func getGradientPoint(axisValue, shift, axisLength int) int {
+	if (axisValue + shift) >= axisLength {
+		return axisLength - axisValue - 1
+	}
+	return shift
+}
+
+func getPixel(x, y, width int) int {
+	return ((y * width) + x) * 4
+}
+
+func Sobel(path string) {
+	img, format, openMS, convertMS := open(path)
+	now := math.Round(float64(time.Now().UnixNano()) / 1000000)
+	width, height := img.Rect.Max.X, img.Rect.Max.Y
+	for i := 0; i < len(img.Pix); i += 4 {
+		x, y := getCoordinates(i/4, width)
+		gradientX := 0
+		gradientY := 0
+		for m := 0; m < 3; m += 1 {
+			for n := 0; n < 3; n += 1 {
+				k := getGradientPoint(x, m, width)
+				l := getGradientPoint(y, n, height)
+				px := getPixel(x+k, y+l, width)
+				average := int((img.Pix[px] + img.Pix[px+1] + img.Pix[px+2]) / 3)
+				gradientX += average * sobelHorizontal[m][n]
+				gradientY += average * sobelVertical[m][n]
+			}
+		}
+		colorCode := 255 - uint8(math.Sqrt(float64(gradientX*gradientX+gradientY*gradientY)))
+		img.Pix[i] = colorCode
+		img.Pix[i+1] = colorCode
+		img.Pix[i+2] = colorCode
+	}
+	processMS := int(math.Round(float64(time.Now().UnixNano())/1000000) - now)
+	saveMS := save(img, format)
+	sum := openMS + convertMS + processMS + saveMS
+	println("open", openMS, "convert", convertMS, "process", processMS, "save", saveMS, "sum", sum)
+}
