c4ad688f2093b7e33e7ac3879b34a43b31fbee19
Peter <p.dyumin@dunice.net>
2023-02-03 17:15:11 +0300

    Optimize Kuwahara filter

--- main.go
+++ main.go
@@ -1,32 +1,23 @@
 package main
 
 import (
-	"fmt"
-	"math"
-	"time"
-
-	"go-image-processing/constants"
 	"go-image-processing/optimized"
-	"go-image-processing/processing"
-	"go-image-processing/utilities"
 )
 
 var FORMAT string
 
 func main() {
-	path := "images/7.jpeg"
-	img, f, openMS, convertMS := utilities.OpenFile(path)
-	FORMAT = f
-	now := math.Round(float64(time.Now().UnixNano()) / 1000000)
+	path := "images/11.jpeg"
+	// img, f, openMS, convertMS := utilities.OpenFile(path)
+	// FORMAT = f
+	// now := math.Round(float64(time.Now().UnixNano()) / 1000000)
 	// rotateN := processing.RotateAngle(img, 52)
-	kuwahara := processing.KuwaharaFilter(img, 5)
 	// gauss := processing.GaussianBlur(img, 25)
-	processMS := int(math.Round(float64(time.Now().UnixNano())/1000000) - now)
+	// processMS := int(math.Round(float64(time.Now().UnixNano())/1000000) - now)
 	// println(est)
-	name := fmt.Sprintf(`file-%d.%s`, time.Now().Unix(), FORMAT)
+	// name := fmt.Sprintf(`file-%d.%s`, time.Now().Unix(), FORMAT)
 	// utilities.SaveFile("rotateN-"+name, FORMAT, rotateN)
-	saveMS := utilities.SaveFile("kuwahara-"+name, FORMAT, kuwahara)
-	// utilities.SaveFile("gauss-"+name, FORMAT, gauss)
+	// saveMS := utilities.SaveFile("gauss-"+name, FORMAT, gauss)
 
 	/* Optimized filters */
 
@@ -41,13 +32,14 @@ func main() {
 	// optimized.Grayscale(path, constants.GRAYSCALE_TYPE_LUMINOSITY)
 	// optimized.HueRotate(path, 252)
 	// optimized.Invert(path)
-	// optimized.Laplacian(path)
-	optimized.RotateFixed(path, constants.ROTATE_FIXED_90)
+	// optimized.Kuwahara(path, 7)
+	optimized.Laplacian(path)
+	// optimized.RotateFixed(path, constants.ROTATE_FIXED_90)
 	// optimized.Sepia(path)
 	// optimized.Sharpen(path, 92)
 	// optimized.Solarize(path, 175)
 	// optimized.Sobel(path)
 
-	sum := openMS + convertMS + processMS + saveMS
-	println("open", openMS, "convert", convertMS, "process", processMS, "save", saveMS, "sum", sum)
+	// sum := openMS + convertMS + processMS + saveMS
+	// println("open", openMS, "convert", convertMS, "process", processMS, "save", saveMS, "sum", sum)
 }
