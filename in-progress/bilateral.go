7311722e1cf0b78115839d4ccf6d8b54e8472fac
Peter <peter.dyumin@gmail.com>
2023-02-04 22:37:46 +0300

    Add Bilateral filter

---
+++ in-progress/bilateral.go
@@ -0,0 +1,80 @@
+package progress
+
+import (
+	"image/color"
+	"math"
+
+	"go-image-processing/utilities"
+)
+
+func distance(x, y, i, j int) float64 {
+	return math.Sqrt(math.Pow(float64(x-i), 2) + math.Pow(float64(y-j), 2))
+}
+
+func gaussian(x int, sigma float64) float64 {
+	return (1.0 / (2 * math.Pi * (math.Pow(sigma, 2)))) * math.Exp(-(math.Pow(float64(x), 2))/(2*math.Pow(sigma, 2)))
+}
+
+func Bilateral(source [][]color.Color) [][]color.Color {
+	width, height := len(source), len(source[0])
+	destination := utilities.CreateGrid(width, height)
+
+	// TODO: these should be arguments
+	radius := 8
+	diameter := radius * 2
+	sigmaI := 12.0
+	sigmaS := 16.0
+
+	for x := 0; x < width; x += 1 {
+		for y := 0; y < height; y += 1 {
+
+			b := 20
+			if x < b || x > width-b || y < b || y > height-b {
+				destination[x][y] = source[x][y]
+				continue
+			}
+
+			filteredR := 0.0
+			filteredG := 0.0
+			filteredB := 0.0
+			WpR := 0.0
+			WpG := 0.0
+			WpB := 0.0
+
+			for i := 0; i < diameter; i += 1 {
+				for j := 0; j < diameter; j += 1 {
+					neighbour_x := x - (radius - i)
+					neighbour_y := y - (radius - j)
+
+					// pi, _ := utilities.Gray(source[x][y])
+					// px, _ := utilities.Gray(source[neighbour_x][neighbour_y])
+
+					pr, pg, pb, _ := utilities.RGBA(source[x][y])
+					nr, ng, nb, _ := utilities.RGBA(source[neighbour_x][neighbour_y])
+
+					giR := gaussian(int(nr-pr), sigmaI)
+					giG := gaussian(int(ng-pg), sigmaI)
+					giB := gaussian(int(nb-pb), sigmaI)
+					gs := gaussian(int(distance(neighbour_x, neighbour_y, x, y)), sigmaS)
+					wR := giR * gs
+					wG := giG * gs
+					wB := giB * gs
+					filteredR += float64(nr) * wR
+					filteredG += float64(ng) * wG
+					filteredB += float64(nb) * wB
+					WpR += wR
+					WpG += wG
+					WpB += wB
+				}
+			}
+			filteredR /= WpR
+			filteredG /= WpG
+			filteredB /= WpB
+			R := uint8(utilities.MaxMin(filteredR, 255, 0))
+			G := uint8(utilities.MaxMin(filteredG, 255, 0))
+			B := uint8(utilities.MaxMin(filteredB, 255, 0))
+			destination[x][y] = color.RGBA{R, G, B, 255}
+		}
+	}
+	return destination
+}
--- main.go
+++ main.go
@@ -1,23 +1,30 @@
 package main
 
 import (
+	"fmt"
+	"go-image-processing/constants"
 	"go-image-processing/filters"
+	progress "go-image-processing/in-progress"
+	"go-image-processing/utilities"
+	"math"
+	"time"
 )
 
 var FORMAT string
 
 func main() {
-	path := "images/11.jpeg"
-	// img, f, openMS, convertMS := utilities.OpenFile(path)
-	// FORMAT = f
-	// now := math.Round(float64(time.Now().UnixNano()) / 1000000)
+	path := "images/5.png"
+	img, f, openMS, convertMS := utilities.OpenFile(path)
+	FORMAT = f
+	now := math.Round(float64(time.Now().UnixNano()) / 1000000)
 	// rotateN := processing.RotateAngle(img, 52)
 	// gauss := processing.GaussianBlur(img, 25)
-	// processMS := int(math.Round(float64(time.Now().UnixNano())/1000000) - now)
-	// println(est)
-	// name := fmt.Sprintf(`file-%d.%s`, time.Now().Unix(), FORMAT)
+	bilateral := progress.Bilateral(img)
+	processMS := int(math.Round(float64(time.Now().UnixNano())/1000000) - now)
+	name := fmt.Sprintf(`file-%d.%s`, time.Now().Unix(), FORMAT)
 	// utilities.SaveFile("rotateN-"+name, FORMAT, rotateN)
 	// saveMS := utilities.SaveFile("gauss-"+name, FORMAT, gauss)
+	saveMS := utilities.SaveFile("bilateral-"+name, FORMAT, bilateral)
 
 	/* Optimized filters */
 
@@ -29,17 +36,17 @@ func main() {
 	// filters.Emboss(path)
 	// filters.Flip(path, constants.FLIP_TYPE_VERTICAL)
 	// filters.GammaCorrection(path, 0.7)
-	// filters.Grayscale(path, constants.GRAYSCALE_TYPE_LUMINOSITY)
+	filters.Grayscale(path, constants.GRAYSCALE_TYPE_AVERAGE)
 	// filters.HueRotate(path, 252)
 	// filters.Invert(path)
 	// filters.Kuwahara(path, 7)
-	filters.Laplacian(path)
+	// filters.Laplacian(path)
 	// filters.RotateFixed(path, constants.ROTATE_FIXED_90)
 	// filters.Sepia(path)
 	// filters.Sharpen(path, 92)
-	// filters.Solarize(path, 175)
 	// filters.Sobel(path)
+	// filters.Solarize(path, 175)
 
-	// sum := openMS + convertMS + processMS + saveMS
-	// println("open", openMS, "convert", convertMS, "process", processMS, "save", saveMS, "sum", sum)
+	sum := openMS + convertMS + processMS + saveMS
+	println("open", openMS, "convert", convertMS, "process", processMS, "save", saveMS, "sum", sum)
 }
